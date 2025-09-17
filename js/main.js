728c025044bf1f37539d7bf9ddc6605a54dd73a1
plantitalabs <plantitalabs>
2025-09-16 23:31:45 -0700

    simpler nav

--- js/main.js
+++ js/main.js
@@ -1,45 +1,11 @@
-// add classes for mobile navigation toggling
-var CSbody = document.querySelector("body");
-var CSappBodyContainer = document.querySelector("#app_body_container");
-const CSnavbarMenu = document.querySelector("#cs-navigation");
-const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
-
-CShamburgerMenu.addEventListener('click', function() {
-    CShamburgerMenu.classList.toggle("cs-active");
-    CSnavbarMenu.classList.toggle("cs-active");
-    CSappBodyContainer.classList.toggle("cs-open");
-    // run the function to check the aria-expanded value
-    ariaExpanded();
-});
-
-// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
-function ariaExpanded() {
-    const csUL = document.querySelector('#cs-expanded');
-    const csExpanded = csUL.getAttribute('aria-expanded');
-
-    if (csExpanded === 'false') {
-        csUL.setAttribute('aria-expanded', 'true');
-    } else {
-        csUL.setAttribute('aria-expanded', 'false');
-    }
-}
-
-// mobile nav toggle code
-const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
-    for (const item of dropDowns) {
-        const onClick = () => {
-        item.classList.toggle('cs-active')
-    }
-    item.addEventListener('click', onClick)
-    }
-
-// after scrolling down 100px, add .scroll class to the #cs-navigation
-document.addEventListener('scroll', (e) => { 
-    const scroll = document.documentElement.scrollTop;
-    if(scroll >= 100){
-document.querySelector('#cs-navigation').classList.add('scroll')
-    } else {
-    document.querySelector('#cs-navigation').classList.remove('scroll')
-    }
-});
-                            
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }         
